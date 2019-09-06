690bca2fb5e27e93254a77056ce5f0daac3f60eb
tylerhipke <tyerhipke@gmail.com>
2019-09-05 21:51:28 -0700

    add post bid function

--- greatBay.js
+++ greatBay.js
@@ -2,58 +2,101 @@ var inquirer = require('inquirer');
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'Grocero8',
-    database : 'greatBay_DB'
-  });
+    host: 'localhost',
+    user: 'root',
+    password: 'Grocero8',
+    database: 'greatBay_DB'
+});
 
-inquirer
-    .prompt([{
-        type: 'list',
-        name: 'userChoice',
-        message: 'Would you like to Post an item or Bid on an item?',
-        choices: ['post', 'bid']
-    }, ])
-    .then(function( answer ) {
-        console.log("userChoice", answer.userChoice);
-        switch (answer.userChoice) {
-            case "post":
-                postItem();
-                break;
-            case "bid":
-                postBid();
-                break;
-            default:
-                console.log("Error: try again");
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    startPrompt();
+});
 
-        }
 
-    });
+function startPrompt() {
+
+    inquirer
+        .prompt([{
+            type: 'list',
+            name: 'userChoice',
+            message: 'Would you like to Post an item or Bid on an item?',
+            choices: ['post', 'bid']
+        }, ])
+        .then(function (answer) {
+            console.log("userChoice", answer.userChoice);
+            switch (answer.userChoice) {
+                case "post":
+                    postItem();
+                    break;
+                case "bid":
+                    postBid();
+                    break;
+                default:
+                    console.log("Error: try again");
+
+            }
+
+        });
+}
 
 function postItem() {
     console.log("post");
     inquirer
-        .prompt([
-            {
+        .prompt([{
                 name: 'item_name',
                 message: 'What is the item called?',
-              },
-              {
-                  name: 'category',
-                  message: 'What is the category!!!!!'
-              },
-              {
-                  name: 'bid',
-                  message: "What is your bid?"
-              }
-            ])
-            .then(function(answer) {
-                console.log(answer);
-                
-            });
+            },
+            {
+                name: 'category',
+                message: 'What is the category!!!!!'
+            },
+            {
+                name: 'starting_bid',
+                message: "What is your bid?"
+            }
+        ])
+        .then(function (answer) {
+            console.log(answer);
+
+            console.log("Inserting a new product...\n");
+            connection.query(
+                "INSERT INTO bids SET ?", {
+                    item_name: answer.item_name,
+                    category: answer.category,
+                    starting_bid: answer.starting_bid
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(res.affectedRows + " bids inserted!\n");
+                    startPrompt();
+                    // Call updateProduct AFTER the INSERT completes
+                }
+            );
+
+        });
 }
 
 function postBid() {
     console.log("bid");
+    console.log("Selecting all bids...\n");
+    connection.query("SELECT * FROM bids", function (err, res) {
+        var userArray = [];
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            userArray.push(res[i].item_name);
+        }
+
+        inquirer
+            .prompt([{
+                type: 'list',
+                name: 'bidSelect',
+                message: 'Which is better?',
+                choices: userArray
+            }, ])
+            .then(function(answer) {
+                console.info('Answer:', answers.bidSelect);
+            });
+    });
 }
--- greatBayDb.sql
+++ greatBayDb.sql
@@ -6,12 +6,11 @@ CREATE TABLE bids (
   item_name VARCHAR(45) NOT NULL,
   category VARCHAR(45) NOT NULL,
   starting_bid INT (10) NOT NULL,
-  highest_bid INT (10) NOT NULL,
   PRIMARY KEY (id)
 );
-INSERT INTO bids (item_name, category, starting_bid, highest_bid)
-VALUES ("Watch", "Electronics", 50, 100);
-INSERT INTO bids (item_name, category, startiong_bid, highest_bid)
-VALUES ("Laptop", "Electronics", 150, 500);
-INSERT INTO bids (item_name, Category, starting_bid, highest_bid)
-VALUES ("Cat", "Pet", 5, 15);
+INSERT INTO bids (item_name, category, starting_bid)
+VALUES ("Watch", "Electronics", 50);
+INSERT INTO bids (item_name, category, startiong_bid)
+VALUES ("Laptop", "Electronics", 150);
+INSERT INTO bids (item_name, Category, starting_bid)
+VALUES ("Cat", "Pet", 5);
